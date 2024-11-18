e9bf948651168a0ef0621c253bd31d0c9863bb59
Gustavo Couto <116754904+codebygustavo@users.noreply.github.com>
2024-11-18 02:39:24 +0000

    new details

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,10 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('myDayTask', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT
 })
 
 try {
